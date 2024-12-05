e0b3c445cf4fa9500b777b8c5d24990f74a14c91
Daniel Castanho <56606508+dCastanho@users.noreply.github.com>
2024-12-05 00:12:00 +0000

    Update Camera.svelte

--- src/lib/Camera.svelte
+++ src/lib/Camera.svelte
@@ -20,7 +20,7 @@
 				video: {
 					width: { ideal: 999999 }, // Request a width of 1920 pixels (Full HD)
 					height: { ideal: 999999 }, // Request a height of 1080 pixels (Full HD)
-					facingMode: { exact: "environment" },
+					// facingMode: { exact: "environment" },
 				},
 			});
 			video.srcObject = stream;
