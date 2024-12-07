48767e80e2aa1b8b803a0b9ce8bc56c4ee5b2cc4
dCastanho <dc@cob.pt>
2024-12-07 19:52:09 +0000

    Better modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -53,20 +53,23 @@
 				To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 				-->
 				<div
-				class="relative rounded-lg bg-white p-4 shadow-xl transition-all left-1/2 -translate-x-1/2 w-4/5 top-12"
+				class="relative rounded-lg bg-white p-4 shadow-xl transition-all left-1/2 -translate-x-1/2 w-4/5 overflow-scroll h-[80vh] top-12"
 		>
-		<div class="w-full h-full flex flex-row flex-wrap">
+		<div class="w-full flex flex-row flex-wrap">
 			{#each cameraState.currentListOfOptions as option}
 			<button onclick={() => push(option)} class="w-1/2 p-1">
 				<img src={option.card}   />
 			</button>
 			{/each}
-			<button
-		class="absolute -top-2 -right-2 bg-red-400 p-1 rounded-md"
-		onclick={cancel}
-	><Xmark />
-</button>
+			
 		</div>
 		</div>
 	</div>
+	<button
+		onclick={cancel}
+		type="button"
+		class="z-50 absolute bottom-12 justify-center w-4/5 -translate-x-1/2 left-1/2 inline-flex items-center rounded-md bg-red-600 px-8 py-3 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+	>
+		Cancel
+	</button>
 </div>
